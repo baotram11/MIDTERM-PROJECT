e1e65f91613821a01d3e23b69be352ac69c2b212
HanYang-NBT <19127588@student.hcmus.edu.vn>
2020-04-16 23:29:44 +0700

    TEST_GitHub 02
    DEMO GITHUB
    
    Co-Authored-By: fminhtu <fminhtu@users.noreply.github.com>

---
+++ TEST_GitHub/TEST_GitHub/TEST-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << endl;
+	cout << "	Let's go!" << endl;
+	cout << "	DEADLINE 20:00 PM 29/04/2020." << endl;
+	cout << endl;
+	system("pause");
+	return 0;
+}
