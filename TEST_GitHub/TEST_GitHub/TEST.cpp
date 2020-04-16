3e9c8dade5d3b65b12e51591f7cb1dd326d78ce6
HanYang-NBT <19127588@student.hcmus.edu.vn>
2020-04-16 23:14:16 +0700

    TEST GitHub

---
+++ TEST_GitHub/TEST_GitHub/TEST.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << endl;
+	cout << "	CSC00002 - PROGRAMMING TECHNIQUES" << endl;
+	cout << "	    MIDTERM PROJECT - POINTERS   " << endl;
+	cout << endl;
+	system("pause");
+	return 0;
+}
